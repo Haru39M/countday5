c8c708135771892221f9cfc3eaa560471715c155
Haru39M <s2071149@s.konan-u.ac.jp>
2021-05-23 21:18:56 +0900

    add

--- app/src/main/java/app/wakayama/harusame/countday5/MainActivity.kt
+++ app/src/main/java/app/wakayama/harusame/countday5/MainActivity.kt
@@ -11,23 +11,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var count = 0
+        var text = ""
+        countText.text = count.toString()
 
         plusButton.setOnClickListener{
-            if(countText.text%3 == 0){
-                countText.setTextColor(Color.parseColor(#00FF00))
-            }else{
-                countText.setTextColor(Color.parseColor(#000000))
-            }
             count++
             countText.text = count.toString()
+            text = countText.text.toString()
+            if(text.toInt()%3 == 0){
+                countText.setTextColor(Color.parseColor("#00FF00"))
+            }else{
+                countText.setTextColor(Color.parseColor("#000000"))
+            }
+
         }
         minusButton.setOnClickListener{
             count--
-            countText.text = count.toStoring()
+            countText.text = count.toString()
+            text = countText.text.toString()
+            if(text.toInt()%3 == 0){
+                countText.setTextColor(Color.parseColor("#00FF00"))
+            }else{
+                countText.setTextColor(Color.parseColor("#000000"))
+            }
+
         }
         timesButton.setOnClickListener{
             count *= 3
             countText.text = count.toString()
+            text = countText.text.toString()
+            if(text.toInt()%3 == 0){
+                countText.setTextColor(Color.parseColor("#00FF00"))
+            }else{
+                countText.setTextColor(Color.parseColor("#000000"))
+            }
+
         }
     }
 }
